b5c927f0e9c51e4222321b872e1cc0670afaf90b
ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>
2020-06-02 11:40:04 +0200

    remove useless annotation
    Signed-off-by: ChamseddineBhd <chamseddine.benhamed@ensi-uma.tn>

--- src/main/java/org/gridsuite/study/server/RestResponseEntityExceptionHandler.java
+++ src/main/java/org/gridsuite/study/server/RestResponseEntityExceptionHandler.java
@@ -2,7 +2,6 @@ package org.gridsuite.study.server;
 
 import static org.gridsuite.study.server.StudyConstants.*;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ServerWebExchange;
 
 @ControllerAdvice
-@RequiredArgsConstructor
 public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {StudyException.class})
