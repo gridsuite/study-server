7b816f3fb8b340d2424dd7558a638c521faed7aa
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2021-03-17 11:38:30 +0100

    maj study.sql
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/main/resources/study.sql
+++ src/main/resources/study.sql
@@ -1,20 +1,4 @@
 
-    create table loadFlowParameters (
-       id uuid not null,
-        balanceType varchar(255),
-        dc boolean,
-        distributedSlack boolean,
-        noGeneratorReactiveLimits boolean,
-        phaseShifterRegulationOn boolean,
-        readSlackBus boolean,
-        simulShunt boolean,
-        transformerVoltageControlOn boolean,
-        twtSplitShuntAdmittance boolean,
-        voltageInitMode varchar(255),
-        writeSlackBus boolean,
-        primary key (id)
-    )
-
     create table loadFlowResult (
        id uuid not null,
         logs TEXT,
@@ -46,13 +30,23 @@
         creationDate timestamp,
         description varchar(255),
         isPrivate boolean,
+        balanceType varchar(255),
+        dc boolean,
+        distributedSlack boolean,
+        noGeneratorReactiveLimits boolean,
+        phaseShifterRegulationOn boolean,
+        readSlackBus boolean,
+        simulShunt boolean,
+        transformerVoltageControlOn boolean,
+        twtSplitShuntAdmittance boolean,
+        voltageInitMode varchar(255),
+        writeSlackBus boolean,
         loadFlowStatus varchar(255),
         networkId varchar(255),
         networkUuid uuid,
         securityAnalysisResultUuid uuid,
         studyName varchar(255) not null,
         userId varchar(255) not null,
-        loadFlowParameters_id uuid,
         loadFlowResultEntity_id uuid,
         primary key (id)
     )
@@ -67,22 +61,17 @@
     )
 create index isPrivate_index on study (isPrivate)
 
-    alter table if exists LoadFlowResultEntity_componentResults
-       add constraint loadFlowResultEntity_componentResults_fk
-       foreign key (LoadFlowResultEntity_id)
+    alter table if exists LoadFlowResultEntity_componentResults 
+       add constraint loadFlowResultEntity_componentResults_fk 
+       foreign key (LoadFlowResultEntity_id) 
        references loadFlowResult
 
-    alter table if exists LoadFlowResultEntity_metrics
-       add constraint loadFlowResultEntity_metrics_fk
-       foreign key (LoadFlowResultEntity_id)
+    alter table if exists LoadFlowResultEntity_metrics 
+       add constraint loadFlowResultEntity_metrics_fk 
+       foreign key (LoadFlowResultEntity_id) 
        references loadFlowResult
 
-    alter table if exists study
-       add constraint loadFlowParameters_id_fk
-       foreign key (loadFlowParameters_id)
-       references loadFlowParameters
-
-    alter table if exists study
-       add constraint loadFlowResult_id_fk
-       foreign key (loadFlowResultEntity_id)
+    alter table if exists study 
+       add constraint loadFlowResult_id_fk 
+       foreign key (loadFlowResultEntity_id) 
        references loadFlowResult
