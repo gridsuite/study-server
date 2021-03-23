0a0bf3edf1620c8fc455349270f7c28ce012917d
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2021-03-23 11:39:17 +0100

    update comment
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/test/java/org/gridsuite/study/server/StudyTest.java
+++ src/test/java/org/gridsuite/study/server/StudyTest.java
@@ -872,7 +872,7 @@ public class StudyTest {
                         .studyPrivate(false)
                         .loadFlowStatus(LoadFlowStatus.NOT_DONE).build()));
 
-        // updateStudy event has been sent
+        // broker message for study rename
         messageLFStatus = output.receive(1000);
         assertEquals("", new String(messageLFStatus.getPayload()));
         headersLFStatus = messageLFStatus.getHeaders();
