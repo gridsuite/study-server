f3d78d1d84df93f6c1a91911be8aa2b4b906f06d
chamseddine <chamseddine.benhamed@ensi-uma.tn>
2021-04-16 16:04:30 +0200

    official documentation
    Signed-off-by: chamseddine <chamseddine.benhamed@ensi-uma.tn>

--- src/main/java/org/gridsuite/study/server/repository/AbstractManuallyAssignedIdentifierEntity.java
+++ src/main/java/org/gridsuite/study/server/repository/AbstractManuallyAssignedIdentifierEntity.java
@@ -16,6 +16,8 @@ import org.springframework.data.domain.Persistable;
 /**
  * @author Chamseddine Benhamed <chamseddine.benhamed at rte-france.com>
  */
+
+// Official documentation: https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.entity-persistence.saving-entites.strategies
 @MappedSuperclass
 public abstract class AbstractManuallyAssignedIdentifierEntity<ID> implements Persistable<ID> {
 
