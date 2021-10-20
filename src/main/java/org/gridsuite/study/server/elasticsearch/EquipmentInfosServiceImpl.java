9daca03fbcfb70de262771ede2d90fdfd118a6d0
Slimane AMAR <slimane.amar@rte-france.com>
2021-10-20 11:29:56 +0200

    Sorting according to a type of equipment (#125)
    Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

--- src/main/java/org/gridsuite/study/server/elasticsearch/EquipmentInfosServiceImpl.java
+++ src/main/java/org/gridsuite/study/server/elasticsearch/EquipmentInfosServiceImpl.java
@@ -8,6 +8,7 @@ package org.gridsuite.study.server.elasticsearch;
 
 import org.elasticsearch.index.query.QueryBuilders;
 import org.gridsuite.study.server.dto.EquipmentInfos;
+import org.gridsuite.study.server.dto.EquipmentType;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
@@ -16,9 +17,11 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.lang.NonNull;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A class to implement elasticsearch indexing
@@ -27,7 +30,7 @@ import java.util.stream.Collectors;
  */
 public class EquipmentInfosServiceImpl implements EquipmentInfosService {
 
-    private static final int PAGE_MAX_SIZE = 10000;
+    private static final int PAGE_MAX_SIZE = 1000;
 
     private final EquipmentInfosRepository equipmentInfosRepository;
 
@@ -56,10 +59,28 @@ public class EquipmentInfosServiceImpl implements EquipmentInfosService {
     @Override
     public List<EquipmentInfos> search(@NonNull final String query) {
         NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
-                .withQuery(QueryBuilders.queryStringQuery(query))
-                .withPageable(PageRequest.of(0, PAGE_MAX_SIZE))
-                .build();
+            .withQuery(QueryBuilders.queryStringQuery(query))
+            .withPageable(PageRequest.of(0, PAGE_MAX_SIZE))
+            .build();
+
         SearchHits<EquipmentInfos> searchHits = elasticsearchOperations.search(nativeSearchQuery, EquipmentInfos.class);
-        return searchHits.stream().map(SearchHit::getContent).collect(Collectors.toList());
+
+        List<EquipmentInfos> infos = searchHits
+            .stream()
+            .map(SearchHit::getContent)
+            .collect(Collectors.toList());
+
+        return Stream.of(
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.SUBSTATION.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.VOLTAGE_LEVEL.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.LINE.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.TWO_WINDINGS_TRANSFORMER.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.THREE_WINDINGS_TRANSFORMER.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> e.getType().equals(EquipmentType.HVDC.name())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> EquipmentType.isInjectionType(e.getType())).sorted(Comparator.comparing(EquipmentInfos::getId)),
+                infos.stream().filter(e -> EquipmentType.isSwitchType(e.getType())).sorted(Comparator.comparing(EquipmentInfos::getId))
+            )
+            .flatMap(e -> e)
+            .collect(Collectors.toList());
     }
 }
