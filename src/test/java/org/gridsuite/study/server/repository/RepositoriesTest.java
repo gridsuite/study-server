a3f70fb6174d7c9c359ee0c1b3a5ade4a1808ca9
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2021-11-17 17:19:36 +0100

    Fix unit tests
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/test/java/org/gridsuite/study/server/repository/RepositoriesTest.java
+++ src/test/java/org/gridsuite/study/server/repository/RepositoriesTest.java
@@ -10,6 +10,7 @@ import com.powsybl.loadflow.LoadFlowParameters;
 import com.powsybl.loadflow.LoadFlowResult;
 import org.apache.commons.collections4.map.HashedMap;
 import org.gridsuite.study.server.dto.LoadFlowStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,16 @@ public class RepositoriesTest {
 
     @Autowired
     StudyCreationRequestRepository studyCreationRequestRepository;
+
+    private void cleanDB() {
+        studyRepository.deleteAll();
+        studyCreationRequestRepository.deleteAll();
+    }
+
+    @Before
+    public void setUp() {
+        cleanDB();
+    }
 
     @Test
     @Transactional
@@ -161,7 +172,7 @@ public class RepositoriesTest {
         savedStudyEntity1Updated.setLoadFlowStatus(LoadFlowStatus.CONVERGED);
         studyRepository.save(savedStudyEntity1Updated);
         savedStudyEntity1Updated = studyRepository.findById(studyEntity1.getId()).get();
-        assert savedStudyEntity1Updated != null;
+        assertNotNull(savedStudyEntity1Updated);
         assertEquals(LoadFlowStatus.CONVERGED, savedStudyEntity1Updated.getLoadFlowStatus());
 
     }
