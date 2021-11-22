ae32004fbdb22def8555dcfd40af7db32fc09b8f
Laurent Garnier <laurent.garnier@rte-france.com>
2021-11-22 17:24:39 +0100

    US #689 shared definition with powsybl-network-conversion-server

--- src/main/java/org/gridsuite/study/server/dto/EquipmentInfos.java
+++ src/main/java/org/gridsuite/study/server/dto/EquipmentInfos.java
@@ -16,6 +16,9 @@ import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.util.Set;
 import java.util.UUID;
@@ -29,6 +32,7 @@ import java.util.UUID;
 @ToString
 @EqualsAndHashCode
 @Document(indexName = "#{@environment.getProperty('index.prefix')}equipments")
+@Setting(settingPath = "elasticsearch_settings.json")
 @TypeAlias(value = "EquipmentInfos")
 public class EquipmentInfos {
     @Id
@@ -37,7 +41,13 @@ public class EquipmentInfos {
     @Field("equipmentId")
     String id;
 
-    @Field("equipmentName")
+    @MultiField(
+        mainField = @Field(name = "equipmentName", type = FieldType.Text),
+        otherFields = {
+            @InnerField(suffix = "fullascii", type = FieldType.Keyword, normalizer = "fullascii"),
+            @InnerField(suffix = "raw", type = FieldType.Keyword)
+        }
+    )
     String name;
 
     @Field("equipmentType")
