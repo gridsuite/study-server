d872181cf71af14acc58733de172cdbf936cac59
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-01-18 14:16:10 +0100

    Fix
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- src/test/java/org/gridsuite/study/server/EquipmentInfosServiceTests.java
+++ src/test/java/org/gridsuite/study/server/EquipmentInfosServiceTests.java
@@ -7,17 +7,8 @@
 package org.gridsuite.study.server;
 
 import com.google.common.collect.Iterables;
-import com.powsybl.commons.datasource.ReadOnlyDataSource;
-import com.powsybl.commons.datasource.ResourceDataSource;
-import com.powsybl.commons.datasource.ResourceSet;
-import com.powsybl.iidm.network.Identifiable;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.xml.XMLImporter;
-import com.powsybl.network.store.iidm.impl.NetworkFactoryImpl;
-import com.powsybl.network.store.iidm.impl.NetworkImpl;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.gridsuite.study.server.dto.EquipmentInfos;
-import org.gridsuite.study.server.dto.EquipmentType;
 import org.gridsuite.study.server.dto.VoltageLevelInfos;
 import org.gridsuite.study.server.elasticsearch.EquipmentInfosService;
 import org.junit.Before;
@@ -32,7 +23,8 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Slimane Amar <slimane.amar at rte-france.com>
@@ -153,25 +145,5 @@ public class EquipmentInfosServiceTests {
         hits = new HashSet<>(equipmentInfosService.search("equipmentType:(LINE) AND equipmentId:(*other*)"));
         assertEquals(1, hits.size());
         assertTrue(hits.contains(otherLineInfos));
-    }
-
-    @Test
-    public void testEquipmentType() {
-        ReadOnlyDataSource dataSource = new ResourceDataSource("testCase", new ResourceSet("", TEST_FILE));
-        Network network = new XMLImporter().importData(dataSource, new NetworkFactoryImpl(), null);
-
-        assertEquals(EquipmentType.SUBSTATION, EquipmentType.getType(network.getSubstation("BBE1AA")));
-        assertEquals(EquipmentType.VOLTAGE_LEVEL, EquipmentType.getType(network.getVoltageLevel("BBE1AA1")));
-        assertEquals(EquipmentType.LOAD, EquipmentType.getType(network.getLoad("BBE1AA1 _load")));
-        assertEquals(EquipmentType.LINE, EquipmentType.getType(network.getLine("BBE1AA1  BBE2AA1  1")));
-        assertEquals(EquipmentType.TWO_WINDINGS_TRANSFORMER, EquipmentType.getType(network.getTwoWindingsTransformer("BBE1AA1  BBE3AA1  2")));
-        assertEquals(EquipmentType.CONFIGURED_BUS, EquipmentType.getType(network.getBusBreakerView().getBus("BBE1AA1 ")));
-    }
-
-    @Test
-    public void testBadEquipmentType() {
-        Identifiable<Network> network = new NetworkFactoryImpl().createNetwork("test", "test");
-        String errorMessage = assertThrows(StudyException.class, () -> EquipmentType.getType(network)).getMessage();
-        assertTrue(errorMessage.contains(String.format("The equipment type : %s is unknown", NetworkImpl.class.getSimpleName())));
     }
 }
