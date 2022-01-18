e1353a71c88d4f268acca975d4eba60dff4ec96f
JacquesBorsenberger <63354128+JacquesBorsenberger@users.noreply.github.com>
2022-01-18 09:59:16 +0100

    Implement delete network modification (#150)
    * implement delete network modification
    
    Signed-off-by: jacques borsenberger <jacques.borsenberger@rte-france.com>

--- src/test/java/org/gridsuite/study/server/StudyTest.java
+++ src/test/java/org/gridsuite/study/server/StudyTest.java
@@ -411,6 +411,8 @@ public class StudyTest {
                         .build(), "build.stopped");
                     return new MockResponse().setResponseCode(200)
                         .addHeader("Content-Type", "application/json; charset=utf-8");
+                } else if (path.matches("/v1/groups/.*/modifications/.*") && request.getMethod().equals("DELETE")) {
+                    return new MockResponse().setResponseCode(200);
                 }
 
                 switch (path) {
@@ -2368,6 +2370,48 @@ public class StudyTest {
         testBuildWithNodeUuid(studyNameUserIdUuid, modificationNode4.getId());
 
         assertEquals(BuildStatus.BUILT_INVALID, networkModificationTreeService.getBuildStatus(modificationNode5.getId()));
+    }
+
+    @Test
+    public void deleteModificationRequest() {
+        createStudy("userId", CASE_UUID, false);
+        UUID studyUuid = studyRepository.findAll().get(0).getId();
+        UUID rootNodeUuid = getRootNodeUuid(studyUuid);
+        NetworkModificationNode modificationNode = createNetworkModificationNode(rootNodeUuid);
+        createNetworkModificationNode(rootNodeUuid);
+        NetworkModificationNode node3 = createNetworkModificationNode(modificationNode.getId());
+        /*  root
+           /   \
+         node  modification node
+                 \
+                node3
+            node is only there to test that when we update modification node, it is not in notifications list
+         */
+
+        webTestClient.delete()
+            .uri("/v1/studies/{studyUuid}/nodes/{nodeUuid}/network-modification/{modificationUuid}", UUID.randomUUID(), modificationNode.getId(), node3.getId())
+            .exchange()
+            .expectStatus().isForbidden();
+
+        UUID modificationUuid = UUID.randomUUID();
+        webTestClient.delete()
+            .uri("/v1/studies/{studyUuid}/nodes/{nodeUuid}/network-modification/{modificationUuid}", studyUuid, modificationNode.getId(), modificationUuid)
+            .exchange()
+            .expectStatus().isOk();
+
+        assertTrue(getRequestsDone(1).stream().anyMatch(
+            r -> r.matches("/v1/groups/" + modificationNode.getNetworkModification() + "/modifications/" + modificationUuid))
+        );
+
+        var res = output.receive(1000);
+        assertEquals(studyUuid, res.getHeaders().get("studyUuid"));
+        var nodesList = res.getHeaders().get("nodes", List.class);
+        assertNotNull(nodesList);
+        /* we have changed modification node, */
+        assertEquals(2, nodesList.size());
+        assertEquals(Set.of(modificationNode.getId(), node3.getId()), new HashSet(nodesList));
+        assertEquals("nodeUpdated", res.getHeaders().get("updateType"));
+
     }
 
     @After
