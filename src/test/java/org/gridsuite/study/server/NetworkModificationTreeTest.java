54beeae7b9ffee4363af2cc93a89e700a871691c
Laurent Garnier <laurent.garnier@rte-france.com>
2022-08-02 10:00:08 +0200

    removal of unused import

--- src/test/java/org/gridsuite/study/server/NetworkModificationTreeTest.java
+++ src/test/java/org/gridsuite/study/server/NetworkModificationTreeTest.java
@@ -61,7 +61,6 @@ import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.Message;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringRunner;
