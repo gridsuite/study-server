b0eb74957e7fe764115bc2a5e9a37a67f847e6e8
Hugo Marcellin <hugo.marcelin@rte-france.com>
2022-08-30 17:05:38 +0200

    Increase coverage

--- src/test/java/org/gridsuite/study/server/StudyTest.java
+++ src/test/java/org/gridsuite/study/server/StudyTest.java
@@ -3952,15 +3952,15 @@ public class StudyTest {
 
         //CaseInfos comparison method
         CaseInfos caseInfos1 = CaseInfos.builder().uuid(UUID.fromString(CASE_UUID_STRING)).name(TEST_FILE_UCTE).format("UCTE").build();
-        CaseInfos caseInfos1Bis = CaseInfos.builder().uuid(UUID.fromString(CASE_UUID_STRING)).name(TEST_FILE_UCTE).format("UCTE").build();
-
         CaseInfos caseInfos2 = CaseInfos.builder().uuid(UUID.fromString(NEW_STUDY_CASE_UUID)).name(CASE_NAME).format("XIIDM").build();
         CaseInfos caseInfos3 = CaseInfos.builder().uuid(UUID.fromString(CASE_UUID_STRING)).name(CASE_NAME).format("XIIDM").build();
         CaseInfos caseInfos4 = CaseInfos.builder().uuid(UUID.fromString(CASE_UUID_STRING)).name(TEST_FILE_UCTE).format("XIIDM").build();
 
         boolean selfComparison = caseInfos1.equals(caseInfos1);
+        boolean differentTypeComparison = caseInfos1.equals("caseInfos1");
 
         assertTrue(selfComparison);
+        assertTrue(!differentTypeComparison);
         assertNotEquals(caseInfos1, null);
         assertNotEquals(caseInfos1, caseInfos2);
         assertNotEquals(caseInfos1, caseInfos3);
