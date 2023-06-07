68e2cd0fe3384da7b4c0a432a8df244a3eebb77e
Etienne Homer <etiennehomer@gmail.com>
2023-06-07 20:17:50 +0200

    Fix get network on root node
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/study/server/service/NetworkMapService.java
+++ src/main/java/org/gridsuite/study/server/service/NetworkMapService.java
@@ -56,7 +56,9 @@ public class NetworkMapService {
         if (substationsIds != null) {
             builder = builder.queryParam(QUERY_PARAM_SUBSTATIONS_IDS, substationsIds);
         }
-        builder = builder.queryParam(QUERY_PARAM_VARIANT_ID, variantId);
+        if (!StringUtils.isBlank(variantId)) {
+            builder = builder.queryParam(QUERY_PARAM_VARIANT_ID, variantId);
+        }
         builder = builder.queryParam(QUERY_PARAM_ELEMENT_TYPE, elementType);
         builder = builder.queryParam(QUERY_PARAM_INFO_TYPE, infoType);
         String url = builder.buildAndExpand(networkUuid).toUriString();
