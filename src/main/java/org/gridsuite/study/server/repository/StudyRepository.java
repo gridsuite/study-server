ad20d5fd95b0e779b24a7f71b33673e6980dec2d
Etienne Homer <etiennehomer@gmail.com>
2023-07-06 18:19:46 +0200

    clean checkstyle
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/study/server/repository/StudyRepository.java
+++ src/main/java/org/gridsuite/study/server/repository/StudyRepository.java
@@ -8,7 +8,6 @@ package org.gridsuite.study.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
