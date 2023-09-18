9da3c3a0ba2c59698b766539434394ffa39ebed7
sBouzols <sylvain.bouzols@gmail.com>
2023-09-18 15:54:21 +0200

    fix(): remove status from payload
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/study/server/notification/NotificationService.java
+++ src/main/java/org/gridsuite/study/server/notification/NotificationService.java
@@ -179,7 +179,7 @@ public class NotificationService {
 
     @PostCompletion
     public void emitStudyChanged(UUID studyUuid, UUID nodeUuid, String updateType, StudyIndexationStatus status) {
-        sendUpdateMessage(MessageBuilder.withPayload(status.toString())
+        sendUpdateMessage(MessageBuilder.withPayload("")
                 .setHeader(HEADER_STUDY_UUID, studyUuid)
                 .setHeader(HEADER_NODE, nodeUuid)
                 .setHeader(HEADER_UPDATE_TYPE, updateType)
