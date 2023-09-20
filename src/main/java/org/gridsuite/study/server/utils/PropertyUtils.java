20b59d8a8cc51c2613dfdb2072a6037fc728d3f0
REHILI Ghazwa Ext <rehiligha@gm0winl628.bureau.si.interne>
2023-09-20 11:06:24 +0200

    delete unused import

--- src/main/java/org/gridsuite/study/server/utils/PropertyUtils.java
+++ src/main/java/org/gridsuite/study/server/utils/PropertyUtils.java
@@ -14,9 +14,6 @@ import org.springframework.beans.BeanWrapperImpl;
 import java.beans.FeatureDescriptor;
 import java.beans.PropertyDescriptor;
 import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
  * @author Jacques Borsenberger <jacques.borsenberger at rte-france.com
