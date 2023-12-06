0c8587b94ff7a48ddc78c888cabc8accf0ddc34f
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-12-06 15:07:38 +0100

    fix: checkstyle
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/org/gridsuite/study/server/NetworkModificationUnitTest.java
+++ src/test/java/org/gridsuite/study/server/NetworkModificationUnitTest.java
@@ -26,7 +26,6 @@ import org.gridsuite.study.server.service.*;
 import org.gridsuite.study.server.service.shortcircuit.ShortCircuitService;
 import org.gridsuite.study.server.utils.TestUtils;
 import org.gridsuite.study.server.utils.elasticsearch.DisableElasticsearch;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
