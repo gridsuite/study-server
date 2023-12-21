d1eb749870a8b74b72122d4afa316665e0459b39
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2023-12-21 18:15:54 +0100

    refacto computations handling
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/main/java/org/gridsuite/study/server/dto/ComputationType.java
+++ src/main/java/org/gridsuite/study/server/dto/ComputationType.java
@@ -1,10 +1,23 @@
 package org.gridsuite.study.server.dto;
 
 public enum ComputationType {
-    LOAD_FLOW,
-    SECURITY_ANALYSIS,
-    SENSITIVITY_ANALYSIS,
-    SHORT_CIRCUIT,
-    VOLTAGE_INITIALIZATION,
-    DYNAMIC_SIMULATION
+    LOAD_FLOW("LoadFlow", "Loadflow"),
+    SECURITY_ANALYSIS("SecurityAnalysis", "Security analysis"),
+    SENSITIVITY_ANALYSIS("SensitivityAnalysis", "Sensitivity analysis"),
+    SHORT_CIRCUIT("ShortCircuitAnalysis", "Short circuit analysis"),
+    VOLTAGE_INITIALIZATION("", "Voltage init"),
+    DYNAMIC_SIMULATION("","Dynamic simulation");
+
+    public final String subReporterKey;
+    public final String label;
+
+    ComputationType(String subReporterKey, String label) {
+        this.subReporterKey = subReporterKey;
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return this.label;
+    }
 }
