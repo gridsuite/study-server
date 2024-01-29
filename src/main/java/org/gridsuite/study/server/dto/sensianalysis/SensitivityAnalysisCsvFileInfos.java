8ed8bfa761f60f801aaf673feb44f1e99c6ff9be
YenguiSeddik <101111441+YenguiSeddik@users.noreply.github.com>
2024-01-29 18:11:03 +0100

    Export sensitivity results as csv (#506)
    Signed-off-by: Seddik Yengui <seddik.yengui@rte-france.com>

---
+++ src/main/java/org/gridsuite/study/server/dto/sensianalysis/SensitivityAnalysisCsvFileInfos.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.gridsuite.study.server.dto.sensianalysis;
+
+import com.powsybl.sensitivity.SensitivityFunctionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * @author Seddik Yengui <seddik.yengui at rte-france.com>
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SensitivityAnalysisCsvFileInfos {
+    private SensitivityFunctionType sensitivityFunctionType;
+    private String resultTab;
+    private List<String> csvHeaders;
+}
