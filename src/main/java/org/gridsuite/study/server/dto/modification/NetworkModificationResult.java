7570976fcbbcd6925ca533f36e6776c6edf0820c
sBouzols <sylvain.bouzols@gmail.com>
2024-02-15 17:40:46 +0100

    refactor(): getImpactedSubstationsIds
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/org/gridsuite/study/server/dto/modification/NetworkModificationResult.java
+++ src/main/java/org/gridsuite/study/server/dto/modification/NetworkModificationResult.java
@@ -12,9 +12,9 @@ import lombok.*;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.gridsuite.study.server.dto.impacts.AbstractBaseImpact;
-import org.gridsuite.study.server.dto.impacts.CollectionElementImpact;
 import org.gridsuite.study.server.dto.impacts.SimpleElementImpact;
 import org.gridsuite.study.server.dto.impacts.AbstractBaseImpact.ImpactType;
 import org.gridsuite.study.server.dto.impacts.SimpleElementImpact.SimpleImpactType;
@@ -50,17 +50,11 @@ public class NetworkModificationResult {
     private List<AbstractBaseImpact> networkImpacts = List.of();
 
     public Set<String> getImpactedSubstationsIds() {
-        Set<String> ids = new TreeSet<>();
-        networkImpacts.stream()
+        return networkImpacts.stream()
+            // ignore DELETION simple impacts here
             .filter(impact -> impact.getImpactType() == ImpactType.SIMPLE && ((SimpleElementImpact) impact).getSimpleImpactType() != SimpleImpactType.DELETION)
-            .forEach(impact -> {
-                if (impact instanceof SimpleElementImpact simpleImpact) {
-                    ids.addAll(simpleImpact.getSubstationIds());
-                } else if (impact instanceof CollectionElementImpact) {
-                    // TODO: Do nothing for now
-                }
-            });
-        return ids;
+            .flatMap(impact -> ((SimpleElementImpact) impact).getSubstationIds().stream())
+            .collect(Collectors.toCollection(TreeSet::new)); // using TreeSet to keep natural order
     }
 
 }
