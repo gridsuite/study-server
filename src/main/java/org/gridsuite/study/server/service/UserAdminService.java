626f95614fb652e9f48b4ec8894915caca90166b
David BRAQUART <david.braquart@rte-france.com>
2024-04-08 12:38:27 +0200

    new rest api
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/UserAdminService.java
+++ src/main/java/org/gridsuite/study/server/service/UserAdminService.java
@@ -10,10 +10,15 @@ package org.gridsuite.study.server.service;
 import org.gridsuite.study.server.RemoteServicesProperties;
 import org.gridsuite.study.server.dto.UserProfileInfos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 import static org.gridsuite.study.server.StudyConstants.*;
 import static org.gridsuite.study.server.StudyException.Type.GET_USER_PROFILE_FAILED;
@@ -24,7 +29,7 @@ import static org.gridsuite.study.server.utils.StudyUtils.handleHttpError;
  */
 @Service
 public class UserAdminService {
-    private static final String USERS_PROFILE_URI = "/users/{sub}/profile";
+    private static final String USER_PROFILES_URI = "/profiles?sub={sub}";
     private final String userAdminServerBaseUri;
     private final RestTemplate restTemplate;
 
@@ -35,10 +40,11 @@ public class UserAdminService {
     }
 
     public UserProfileInfos getUserProfile(String sub) {
-        String path = UriComponentsBuilder.fromPath(DELIMITER + LOADFLOW_API_VERSION + USERS_PROFILE_URI)
+        String path = UriComponentsBuilder.fromPath(DELIMITER + LOADFLOW_API_VERSION + USER_PROFILES_URI)
             .buildAndExpand(sub).toUriString();
         try {
-            return restTemplate.getForObject(userAdminServerBaseUri + path, UserProfileInfos.class);
+            List<UserProfileInfos> response = restTemplate.exchange(userAdminServerBaseUri + path, HttpMethod.GET, null, new ParameterizedTypeReference<List<UserProfileInfos>>() { }).getBody();
+            return CollectionUtils.isEmpty(response) ? null : response.get(0);
         } catch (HttpStatusCodeException e) {
             throw handleHttpError(e, GET_USER_PROFILE_FAILED);
         }
