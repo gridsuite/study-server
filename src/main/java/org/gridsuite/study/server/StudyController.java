12d9d66074338f6da07b8402a96b07824446faa3
David BRAQUART <david.braquart@rte-france.com>
2024-04-17 18:35:12 +0200

    use GET /contingency-lists/count
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/main/java/org/gridsuite/study/server/StudyController.java
+++ src/main/java/org/gridsuite/study/server/StudyController.java
@@ -836,8 +836,10 @@ public class StudyController {
     public ResponseEntity<Integer> getContingencyCount(@Parameter(description = "Study UUID") @PathVariable("studyUuid") UUID studyUuid,
                                                              @Parameter(description = "Node UUID") @PathVariable("nodeUuid") UUID nodeUuid,
                                                              @Parameter(description = "Contingency list names") @RequestParam(name = "contingencyListName", required = false) List<String> contingencyListNames) {
-        List<String> nonNullContingencyListNames = contingencyListNames != null ? contingencyListNames : Collections.emptyList();
-        return ResponseEntity.ok().body(studyService.getContingencyCount(studyUuid, nonNullContingencyListNames, nodeUuid));
+        if (CollectionUtils.isEmpty(contingencyListNames)) {
+            return ResponseEntity.ok().body(0);
+        }
+        return ResponseEntity.ok().body(studyService.getContingencyCount(studyUuid, contingencyListNames, nodeUuid));
     }
 
     @GetMapping(value = "/studies/{studyUuid}/nodes/{nodeUuid}/limit-violations")
