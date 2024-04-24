e76cfc7d661b1ee97b58bffbc58402e0a6fe9bff
jamal-khey <myjamal89@gmail.com>
2024-04-24 14:44:47 +0200

    remove unused annotation
    Signed-off-by: jamal-khey <myjamal89@gmail.com>

--- src/main/java/org/gridsuite/study/server/dto/InfoTypesParameters.java
+++ src/main/java/org/gridsuite/study/server/dto/InfoTypesParameters.java
@@ -2,13 +2,11 @@ package org.gridsuite.study.server.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 
 @Data
 @Builder
 public class InfoTypesParameters {
-    @Value("#{${additionalParams: {}}}")
     private Map<String, String> additionalParams;
 }
