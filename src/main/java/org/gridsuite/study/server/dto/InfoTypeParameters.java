e5a8e790bf101d5d899d768bbf285407abc27df7
jamal-khey <myjamal89@gmail.com>
2024-04-29 14:04:18 +0200

    fix style
    Signed-off-by: jamal-khey <myjamal89@gmail.com>

--- src/main/java/org/gridsuite/study/server/dto/InfoTypeParameters.java
+++ src/main/java/org/gridsuite/study/server/dto/InfoTypeParameters.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.study.server.dto;
 
 import lombok.Builder;
@@ -13,6 +19,7 @@ public class InfoTypeParameters {
     public static final String QUERY_PARAM_OPERATION = "operation";
     private String infoType;
     private Map<String, String> optionalParameters;
+
     public InfoTypeParameters(String infoType, Map<String, String> optionalParameters) {
         this.infoType = infoType;
         this.optionalParameters = optionalParameters == null ? new HashMap<>() : optionalParameters;
