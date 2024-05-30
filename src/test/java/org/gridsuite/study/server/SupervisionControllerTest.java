909a0c83f1e9d983e28e19fc46bcec7f411b4096
sBouzols <sylvain.bouzols@gmail.com>
2024-05-30 15:33:04 +0200

    fix unused import
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/test/java/org/gridsuite/study/server/SupervisionControllerTest.java
+++ src/test/java/org/gridsuite/study/server/SupervisionControllerTest.java
@@ -38,7 +38,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.io.IOException;
 import java.util.*;
 
 import static org.junit.Assert.*;
