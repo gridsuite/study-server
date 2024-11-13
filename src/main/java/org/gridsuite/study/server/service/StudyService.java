77e0d670174f88673997032175a0546f943d2d06
souissimai <133104748+souissimai@users.noreply.github.com>
2024-11-13 13:10:07 +0100

    Fetch substation id for voltage level (#628)
    * get substation ID for a given network and given voltage level
    ---------
    
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>
    Signed-off-by: Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>

--- src/main/java/org/gridsuite/study/server/service/StudyService.java
+++ src/main/java/org/gridsuite/study/server/service/StudyService.java
@@ -1256,6 +1256,12 @@ public class StudyService {
         notificationService.emitElementUpdated(studyUuid, userId);
     }
 
+    public String getVoltageLevelSubstationId(UUID studyUuid, UUID nodeUuid, String voltageLevelId) {
+        UUID networkUuid = networkStoreService.getNetworkUuid(studyUuid);
+        String variantId = networkModificationTreeService.getVariantId(nodeUuid, self.getStudyFirstRootNetworkUuid(studyUuid));
+        return networkMapService.getVoltageLevelSubstationId(networkUuid, variantId, voltageLevelId);
+    }
+
     public List<IdentifiableInfos> getVoltageLevelBusesOrBusbarSections(UUID studyUuid, UUID nodeUuid, String voltageLevelId,
                                                                         String busPath) {
         UUID networkUuid = networkStoreService.getNetworkUuid(studyUuid);
@@ -1267,6 +1273,11 @@ public class StudyService {
     public List<IdentifiableInfos> getVoltageLevelBuses(UUID studyUuid, UUID nodeUuid, String voltageLevelId, boolean inUpstreamBuiltParentNode) {
         UUID nodeUuidToSearchIn = getNodeUuidToSearchIn(nodeUuid, self.getStudyFirstRootNetworkUuid(studyUuid), inUpstreamBuiltParentNode);
         return getVoltageLevelBusesOrBusbarSections(studyUuid, nodeUuidToSearchIn, voltageLevelId, "configured-buses");
+    }
+
+    public String getVoltageLevelSubstationId(UUID studyUuid, UUID nodeUuid, String voltageLevelId, boolean inUpstreamBuiltParentNode) {
+        UUID nodeUuidToSearchIn = getNodeUuidToSearchIn(nodeUuid, self.getStudyFirstRootNetworkUuid(studyUuid), inUpstreamBuiltParentNode);
+        return getVoltageLevelSubstationId(studyUuid, nodeUuidToSearchIn, voltageLevelId);
     }
 
     public List<IdentifiableInfos> getVoltageLevelBusbarSections(UUID studyUuid, UUID nodeUuid, String voltageLevelId, boolean inUpstreamBuiltParentNode) {
