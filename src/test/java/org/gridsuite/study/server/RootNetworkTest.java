890974a7f3d9a903cb79370b6b4e5eca87a1a03f
maissa SOUISSI <maissa.souissi@rte-france.com>
2024-12-04 16:44:53 +0100

    fixes
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/test/java/org/gridsuite/study/server/RootNetworkTest.java
+++ src/test/java/org/gridsuite/study/server/RootNetworkTest.java
@@ -37,7 +37,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.web.servlet.MockMvc;
