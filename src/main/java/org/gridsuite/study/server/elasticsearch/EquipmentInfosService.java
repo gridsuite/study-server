4a4f54ce233dfc0b2469ea4b1b67f105356f5777
Antoine Bouhours <antoine.bouhours@rte-france.com>
2025-02-07 10:56:50 +0100

    Revert b275ff8, method has to be public for tests

--- src/main/java/org/gridsuite/study/server/elasticsearch/EquipmentInfosService.java
+++ src/main/java/org/gridsuite/study/server/elasticsearch/EquipmentInfosService.java
@@ -120,7 +120,7 @@ public class EquipmentInfosService {
         return tombstonedEquipmentInfosRepository.findAllByNetworkUuid(networkUuid);
     }
 
-    private Set<TombstonedEquipmentInfos> findTombstonedEquipmentInfosByIdIn(@NonNull UUID networkUuid, @NonNull String variantId, @NonNull List<String> equipmentIds) {
+    public Set<TombstonedEquipmentInfos> findTombstonedEquipmentInfosByIdIn(@NonNull UUID networkUuid, @NonNull String variantId, @NonNull List<String> equipmentIds) {
         return tombstonedEquipmentInfosRepository.findByIdInAndNetworkUuidAndVariantId(
                         equipmentIds,
                         networkUuid,
