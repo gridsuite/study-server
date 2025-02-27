807cbc781357020b524d5f749443d2f756dca68f
Hugo Marcellin <marcellinhugo@orange.fr>
2025-02-27 17:36:47 +0100

    Invalidate state estimation result on param update (#697)
    * Invalidate State estimation result on param update
    
    Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>
    Co-authored-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/StudyService.java
+++ src/main/java/org/gridsuite/study/server/service/StudyService.java
@@ -1205,6 +1205,10 @@ public class StudyService {
         voltageInitService.invalidateVoltageInitStatus(rootNetworkNodeInfoService.getComputationResultUuids(studyUuid, VOLTAGE_INITIALIZATION));
     }
 
+    public void invalidateStateEstimationStatusOnAllNodes(UUID studyUuid) {
+        stateEstimationService.invalidateStateEstimationStatus(rootNetworkNodeInfoService.getComputationResultUuids(studyUuid, STATE_ESTIMATION));
+    }
+
     private StudyEntity updateStudyIndexationStatus(StudyEntity studyEntity, StudyIndexationStatus indexationStatus) {
         studyEntity.setIndexationStatus(indexationStatus);
         notificationService.emitStudyIndexationStatusChanged(studyEntity.getId(), indexationStatus);
@@ -2704,6 +2708,7 @@ public class StudyService {
         } else {
             stateEstimationService.updateStateEstimationParameters(existingStateEstimationParametersUuid, parameters);
         }
+        invalidateStateEstimationStatusOnAllNodes(studyEntity.getId());
     }
 
     @Transactional
