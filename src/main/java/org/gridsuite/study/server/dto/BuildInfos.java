b581518e429b8a91e63a571c7622d01a198aeb4b
Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>
2025-03-05 15:50:39 +0100

    Use a build context
    Signed-off-by: Slimane AMAR <amarsli@gm0winl104.bureau.si.interne>

--- src/main/java/org/gridsuite/study/server/dto/BuildInfos.java
+++ src/main/java/org/gridsuite/study/server/dto/BuildInfos.java
@@ -10,8 +10,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.gridsuite.study.server.dto.modification.BuildContext;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
@@ -25,18 +28,17 @@ public class BuildInfos {
 
     private String destinationVariantId;
 
-    private List<UUID> modificationGroupUuids = new ArrayList<>();
+    private List<BuildContext> buildContextsInfos = new ArrayList<>();
 
-    // map with modification groups as key, modification to excludes as value
-    private Map<UUID, Set<UUID>> modificationUuidsToExclude = new HashMap<>();
+    private Stream<ReportInfos> getReportsInfos() {
+        return buildContextsInfos.stream().map(BuildContext::reportInfos);
+    }
 
-    private List<ReportInfos> reportsInfos = new ArrayList<>();
+    public Map<UUID, UUID> getReportsMappings() {
+        return getReportsInfos().collect(Collectors.toMap(ReportInfos::nodeUuid, ReportInfos::reportUuid));
+    }
 
-    public void insertModificationInfos(UUID modificationGroupUuid, Set<UUID> modificationUuidsToExclude, ReportInfos reportInfos) {
-        if (modificationUuidsToExclude != null && !modificationUuidsToExclude.isEmpty()) {
-            this.modificationUuidsToExclude.put(modificationGroupUuid, modificationUuidsToExclude);
-        }
-        modificationGroupUuids.add(0, modificationGroupUuid);
-        reportsInfos.add(0, reportInfos);
+    public void addApplicationContextInfos(UUID modificationGroupUuid, Set<UUID> modificationUuidsToExclude, ReportInfos reportInfos) {
+        buildContextsInfos.add(0, new BuildContext(modificationGroupUuid, modificationUuidsToExclude == null ? new HashSet<>() : modificationUuidsToExclude, reportInfos));
     }
 }
