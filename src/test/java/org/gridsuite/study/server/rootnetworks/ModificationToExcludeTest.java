3dfd4255de778c045d008c91cdf148fb8260b66d
AAJELLAL <ali.ajellalod@gmail.com>
2025-04-18 14:50:22 +0200

    Some naming changes.
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/test/java/org/gridsuite/study/server/rootnetworks/ModificationToExcludeTest.java
+++ src/test/java/org/gridsuite/study/server/rootnetworks/ModificationToExcludeTest.java
@@ -399,7 +399,7 @@ class ModificationToExcludeTest {
 
         // RUN study tree duplication
         UUID newStudyUuid = UUID.randomUUID();
-        studyCreationRequestRepository.save(new StudyCreationRequestEntity(newStudyUuid, "caseName"));
+        studyCreationRequestRepository.save(new StudyCreationRequestEntity(newStudyUuid, "firstRootNetworkName"));
         studyService.duplicateStudyAsync(BasicStudyInfos.builder().id(newStudyUuid).build(), studyEntity.getId(), USER_ID);
 
         List<BasicRootNetworkInfos> newStudyRootNetworkBasicInfos = studyService.getExistingBasicRootNetworkInfos(newStudyUuid);
