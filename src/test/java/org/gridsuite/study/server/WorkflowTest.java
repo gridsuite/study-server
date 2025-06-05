ff37cd137c361889cf968033ca19c5807c95949b
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-06-05 15:56:53 +0200

    test: coverage
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/org/gridsuite/study/server/WorkflowTest.java
+++ src/test/java/org/gridsuite/study/server/WorkflowTest.java
@@ -6,6 +6,7 @@ import org.gridsuite.study.server.dto.NodeReceiver;
 import org.gridsuite.study.server.dto.modification.NetworkModificationResult;
 import org.gridsuite.study.server.dto.workflow.RerunLoadFlowWorkflowInfos;
 import org.gridsuite.study.server.dto.workflow.WorkflowType;
+import org.gridsuite.study.server.notification.NotificationService;
 import org.gridsuite.study.server.service.ConsumerService;
 import org.gridsuite.study.server.service.NetworkModificationTreeService;
 import org.gridsuite.study.server.service.StudyService;
@@ -42,9 +43,11 @@ class WorkflowTest {
     private NetworkModificationTreeService networkModificationTreeService;
     @MockBean
     private StudyService studyService;
+    @MockBean
+    private NotificationService notificationService;
 
     @Test
-    void testConsumeBuildInRerunLoadFlowWorkflow() throws JsonProcessingException {
+    void testConsumeBuildResultInRerunLoadFlowWorkflow() throws JsonProcessingException {
         boolean withRatioTapChangers = true;
         NetworkModificationResult networkModificationResult = new NetworkModificationResult();
         NodeReceiver nodeReceiver = new NodeReceiver(nodeUuid, rootNetworkUuid);
@@ -68,5 +71,33 @@ class WorkflowTest {
         // check loadflow is actually ran after build is completed
         verify(studyService, times(1)).handleBuildSuccess(studyUuid, nodeUuid, rootNetworkUuid, networkModificationResult);
         verify(studyService, times(1)).sendLoadflowRequest(studyUuid, nodeUuid, rootNetworkUuid, loadflowResultUuid, withRatioTapChangers, userId);
+    }
+
+    @Test
+    void testConsumeBuildFailedInRerunLoadFlowWorkflow() throws JsonProcessingException {
+        boolean withRatioTapChangers = true;
+        NodeReceiver nodeReceiver = new NodeReceiver(nodeUuid, rootNetworkUuid);
+        RerunLoadFlowWorkflowInfos rerunLoadFlowWorkflowInfos = RerunLoadFlowWorkflowInfos.builder()
+            .loadflowResultUuid(loadflowResultUuid)
+            .withRatioTapChangers(withRatioTapChangers)
+            .userId(userId)
+            .build();
+
+        Map<String, Object> headers = new HashMap<>();
+        headers.put(HEADER_RECEIVER, objectMapper.writeValueAsString(nodeReceiver));
+        String errorMessage = "Build failure";
+        headers.put(HEADER_ERROR_MESSAGE, errorMessage);
+        headers.put(HEADER_WORKFLOW_TYPE, WorkflowType.RERUN_LOAD_FLOW.name());
+        headers.put(HEADER_WORKFLOW_INFOS, rerunLoadFlowWorkflowInfos.serialize(objectMapper));
+        MessageHeaders messageHeaders = new MessageHeaders(headers);
+
+        when(networkModificationTreeService.getStudyUuidForNodeId(nodeUuid)).thenReturn(studyUuid);
+
+        // execute consume
+        consumerService.consumeBuildFailed().accept(MessageBuilder.createMessage("", messageHeaders));
+
+        // check loadflow is actually ran after build is completed
+        verify(notificationService, times(1)).emitNodeBuildFailed(studyUuid, nodeUuid, rootNetworkUuid, errorMessage);
+        verify(studyService, times(1)).deleteLoadflowResult(studyUuid, nodeUuid, rootNetworkUuid, loadflowResultUuid, withRatioTapChangers);
     }
 }
