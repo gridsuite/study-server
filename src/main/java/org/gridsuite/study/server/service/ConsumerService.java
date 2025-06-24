4020e8adcaf061e3004af66c427bdcc72339dd29
Thang PHAM <phamthang37@gmail.com>
2025-06-24 09:24:38 +0200

    create debug chanel
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/org/gridsuite/study/server/service/ConsumerService.java
+++ src/main/java/org/gridsuite/study/server/service/ConsumerService.java
@@ -56,7 +56,6 @@ public class ConsumerService {
     static final String HEADER_CASE_FORMAT = "caseFormat";
     static final String HEADER_CASE_NAME = "caseName";
     static final String HEADER_ERROR_MESSAGE = "errorMessage";
-    static final String HEADER_DEBUG = "debug";
 
     private final ObjectMapper objectMapper;
 
@@ -577,22 +576,22 @@ public class ConsumerService {
         }
     }
 
-    public void consumeCalculationResult(Message<String> msg, ComputationType computationType) {
+    public void consumeCalculationDebug(Message<String> msg, ComputationType computationType) {
         Optional.ofNullable(msg.getHeaders().get(RESULT_UUID, String.class))
             .map(UUID::fromString)
             .ifPresent(resultUuid -> getNodeReceiver(msg).ifPresent(receiverObj -> {
                 UUID studyUuid = networkModificationTreeService.getStudyUuidForNodeId(receiverObj.getNodeUuid());
 
-                // process debug notification which shares the same result chanel
-                // TODO whether need create a new debug channel
-                var debug = msg.getHeaders().get(HEADER_DEBUG);
-                if (Boolean.TRUE.equals(debug)) {
-                    String errorMessage = (String) msg.getHeaders().get(HEADER_ERROR_MESSAGE);
-                    String userId = (String) msg.getHeaders().get(HEADER_USER_ID);
-                    notificationService.emitComputationDebugFileStatus(studyUuid, receiverObj.getNodeUuid(), receiverObj.getRootNetworkUuid(), computationType, userId, resultUuid, errorMessage);
-                    return;
-                }
+                String errorMessage = (String) msg.getHeaders().get(HEADER_ERROR_MESSAGE);
+                String userId = (String) msg.getHeaders().get(HEADER_USER_ID);
+                notificationService.emitComputationDebugFileStatus(studyUuid, receiverObj.getNodeUuid(), receiverObj.getRootNetworkUuid(), computationType, userId, resultUuid, errorMessage);
+            }));
+    }
 
+    public void consumeCalculationResult(Message<String> msg, ComputationType computationType) {
+        Optional.ofNullable(msg.getHeaders().get(RESULT_UUID, String.class))
+            .map(UUID::fromString)
+            .ifPresent(resultUuid -> getNodeReceiver(msg).ifPresent(receiverObj -> {
                 LOGGER.info("{} result '{}' available for node '{}'",
                     computationType.getLabel(),
                     resultUuid,
@@ -600,6 +599,8 @@ public class ConsumerService {
 
                 // update DB
                 rootNetworkNodeInfoService.updateComputationResultUuid(receiverObj.getNodeUuid(), receiverObj.getRootNetworkUuid(), resultUuid, computationType);
+
+                UUID studyUuid = networkModificationTreeService.getStudyUuidForNodeId(receiverObj.getNodeUuid());
 
                 if (computationType == LOAD_FLOW) {
                     // since running loadflow impacts the network linked to the node "nodeUuid", we need to invalidate its children nodes to prevent inconsistencies
@@ -631,6 +632,11 @@ public class ConsumerService {
     }
 
     @Bean
+    public Consumer<Message<String>> consumeDsDebug() {
+        return message -> consumeCalculationDebug(message, DYNAMIC_SIMULATION);
+    }
+
+    @Bean
     public Consumer<Message<String>> consumeDsResult() {
         return message -> consumeCalculationResult(message, DYNAMIC_SIMULATION);
     }
@@ -643,6 +649,11 @@ public class ConsumerService {
     @Bean
     public Consumer<Message<String>> consumeDsFailed() {
         return message -> consumeCalculationFailed(message, DYNAMIC_SIMULATION);
+    }
+
+    @Bean
+    public Consumer<Message<String>> consumeDsaDebug() {
+        return message -> consumeCalculationDebug(message, DYNAMIC_SECURITY_ANALYSIS);
     }
 
     @Bean
