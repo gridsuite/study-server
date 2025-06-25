fe5690b4b834809cb3c65544676e41e8aac980a0
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-06-25 10:59:37 +0200

    fix: PR remarks
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/org/gridsuite/study/server/dto/sequence/SecuritySequence.java
+++ src/main/java/org/gridsuite/study/server/dto/sequence/SecuritySequence.java
@@ -1,7 +1,16 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.study.server.dto.sequence;
 
 import org.gridsuite.study.server.networkmodificationtree.entities.NetworkModificationNodeType;
 
+/**
+ * @author Kevin Le Saulnier <kevin.lesaulnier at rte-france.com>
+ */
 public final class SecuritySequence {
     public static final String N_NODE_NAME = "N";
     public static final String NMK_NODE_NAME = "N-K";
