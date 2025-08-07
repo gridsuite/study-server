17205791b8d39617b2b4c19ba6b1d03c693f7aa8
AAJELLAL <ali.ajellalod@gmail.com>
2025-08-07 16:55:51 +0200

    fix checkstyle
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/main/java/org/gridsuite/study/server/controller/StudyController.java
+++ src/main/java/org/gridsuite/study/server/controller/StudyController.java
@@ -6,7 +6,6 @@
  */
 package org.gridsuite.study.server.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.powsybl.iidm.network.ThreeSides;
 import com.powsybl.timeseries.DoubleTimeSeries;
