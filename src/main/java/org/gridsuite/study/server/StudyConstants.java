ee669cac42cb8630fce052a00f214fa367884aa9
dbraquart <107846716+dbraquart@users.noreply.github.com>
2025-09-03 10:48:55 +0200

    Set proper Nad name when we create a new study with a Nad in user profile (#836)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/main/java/org/gridsuite/study/server/StudyConstants.java
+++ src/main/java/org/gridsuite/study/server/StudyConstants.java
@@ -36,6 +36,7 @@ public final class StudyConstants {
     public static final String DYNAMIC_MAPPING_API_VERSION = ""; // mapping server is now without version, must be v1 in the next time
     public static final String FILTER_API_VERSION = "v1";
     public static final String STATE_ESTIMATION_API_VERSION = "v1";
+    public static final String DIRECTORY_API_VERSION = "v1";
 
     public static final String NETWORK_UUID = "networkUuid";
     public static final String CASE_UUID = "caseUuid";
