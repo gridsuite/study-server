cbbcd23bc3b4b3a78b6c009ea4bc5f381e49b5da
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2025-11-19 18:34:45 +0100

    download csv results with sort filter , without pagination
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/test/java/org/gridsuite/study/server/utils/WireMockUtils.java
+++ src/test/java/org/gridsuite/study/server/utils/WireMockUtils.java
@@ -706,6 +706,19 @@ public class WireMockUtils {
             ));
     }
 
+    public void verifyExportPccMinResult(UUID stubId, UUID resultUuid) {
+        verifyPostRequest(
+            stubId,
+            "/v1/results/" + resultUuid + "/csv",
+            Map.of(
+                "sort", WireMock.equalTo("id,DESC"),
+                "filters", WireMock.equalTo("fakeFilters"),
+                "globalFilters", WireMock.equalTo("fakeGlobalFilters")
+            ),
+            1
+        );
+    }
+
     public UUID stubPccMinParametersGet(String paramUuid, String responseBody) {
         return wireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/v1/parameters/" + paramUuid))
             .willReturn(WireMock.ok().withBody(responseBody))
