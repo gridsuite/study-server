fbc2a6e19a4ca109f5e5de70eed244244892454a
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-12-09 13:53:03 +0100

    Fix sonar maintainability issue
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/StudyService.java
+++ src/main/java/org/gridsuite/study/server/service/StudyService.java
@@ -16,6 +16,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.gridsuite.filter.globalfilter.GlobalFilter;
 import org.gridsuite.filter.utils.EquipmentType;
 import org.gridsuite.study.server.StudyConstants;
+import org.gridsuite.study.server.dto.voltageinit.ContextInfos;
 import org.gridsuite.study.server.error.StudyException;
 import org.gridsuite.study.server.dto.*;
 import org.gridsuite.study.server.dto.InvalidateNodeTreeParameters.ComputationsInvalidationMode;
@@ -2705,7 +2706,7 @@ public class StudyService {
 
         UUID result = voltageInitService.runVoltageInit(new VariantInfos(networkUuid, variantId), studyEntity.getVoltageInitParametersUuid(),
                                                         new ReportInfos(reportUuid, nodeUuid), rootNetworkUuid, userId, debug,
-                                                        rootNetwork.getName(), nodeEntity.getName());
+                                                        new ContextInfos(rootNetwork.getName(), nodeEntity.getName()));
 
         updateComputationResultUuid(nodeUuid, rootNetworkUuid, result, VOLTAGE_INITIALIZATION);
         notificationService.emitStudyChanged(studyEntity.getId(), nodeUuid, rootNetworkUuid, NotificationService.UPDATE_TYPE_VOLTAGE_INIT_STATUS);
--- src/main/java/org/gridsuite/study/server/service/VoltageInitService.java
+++ src/main/java/org/gridsuite/study/server/service/VoltageInitService.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.gridsuite.study.server.RemoteServicesProperties;
+import org.gridsuite.study.server.dto.voltageinit.ContextInfos;
 import org.gridsuite.study.server.error.StudyException;
 import org.gridsuite.study.server.dto.NodeReceiver;
 import org.gridsuite.study.server.dto.ReportInfos;
@@ -59,8 +60,7 @@ public class VoltageInitService extends AbstractComputationService {
         this.objectMapper = objectMapper;
     }
 
-    public UUID runVoltageInit(VariantInfos variantInfos, UUID parametersUuid, ReportInfos reportInfos, UUID rootNetworkUuid, String userId, boolean debug,
-                               String rootNetworkName, String nodeName) {
+    public UUID runVoltageInit(VariantInfos variantInfos, UUID parametersUuid, ReportInfos reportInfos, UUID rootNetworkUuid, String userId, boolean debug, ContextInfos contextInfos) {
         String receiver;
         try {
             receiver = URLEncoder.encode(objectMapper.writeValueAsString(new NodeReceiver(reportInfos.nodeUuid(), rootNetworkUuid)), StandardCharsets.UTF_8);
@@ -87,8 +87,8 @@ public class VoltageInitService extends AbstractComputationService {
             uriComponentsBuilder.queryParam(QUERY_PARAM_DEBUG, true);
         }
 
-        uriComponentsBuilder.queryParam(QUERY_PARAM_ROOT_NETWORK_NAME, rootNetworkName);
-        uriComponentsBuilder.queryParam(QUERY_PARAM_NODE_NAME, nodeName);
+        uriComponentsBuilder.queryParam(QUERY_PARAM_ROOT_NETWORK_NAME, contextInfos.rootNetworkName());
+        uriComponentsBuilder.queryParam(QUERY_PARAM_NODE_NAME, contextInfos.nodeName());
 
         var path = uriComponentsBuilder.buildAndExpand(variantInfos.getNetworkUuid()).toUriString();
 
