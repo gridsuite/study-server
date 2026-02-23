74e064aeab6ee7342bdfa092e51101f8b33731ce
SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>
2026-02-23 13:33:05 +0100

    duplicated code : dto
    Signed-off-by: SOUISSI Maissa (Externe) <souissimai@gm0winl878.bureau.si.interne>

--- src/main/java/org/gridsuite/study/server/dto/DeleteNodeInfos.java
+++ src/main/java/org/gridsuite/study/server/dto/DeleteNodeInfos.java
@@ -19,73 +19,12 @@ import java.util.*;
 @NoArgsConstructor
 @Getter
 @Setter
-public class DeleteNodeInfos {
+public class DeleteNodeInfos extends NodeInfos {
     private List<UUID> removedNodeUuids = new ArrayList<>();
 
     private List<UUID> modificationGroupUuids = new ArrayList<>();
 
     private Map<UUID, List<String>> variantIds = new HashMap<>();
-
-    private Set<UUID> reportUuids = new HashSet<>();
-    private Set<UUID> loadFlowResultUuids = new HashSet<>();
-    private Set<UUID> securityAnalysisResultUuids = new HashSet<>();
-    private Set<UUID> sensitivityAnalysisResultUuids = new HashSet<>();
-    private Set<UUID> shortCircuitAnalysisResultUuids = new HashSet<>();
-    private Set<UUID> oneBusShortCircuitAnalysisResultUuids = new HashSet<>();
-    private Set<UUID> voltageInitResultUuids = new HashSet<>();
-    private Set<UUID> dynamicSimulationResultUuids = new HashSet<>();
-    private Set<UUID> dynamicSecurityAnalysisResultUuids = new HashSet<>();
-    private Set<UUID> dynamicMarginCalculationResultUuids = new HashSet<>();
-    private Set<UUID> stateEstimationResultUuids = new HashSet<>();
-    private Set<UUID> pccMinResultUuids = new HashSet<>();
-
-    public List<UUID> getReportUuids() {
-        return reportUuids.stream().toList();
-    }
-
-    public List<UUID> getLoadFlowResultUuids() {
-        return loadFlowResultUuids.stream().toList();
-    }
-
-    public List<UUID> getSecurityAnalysisResultUuids() {
-        return securityAnalysisResultUuids.stream().toList();
-    }
-
-    public List<UUID> getSensitivityAnalysisResultUuids() {
-        return sensitivityAnalysisResultUuids.stream().toList();
-    }
-
-    public List<UUID> getShortCircuitAnalysisResultUuids() {
-        return shortCircuitAnalysisResultUuids.stream().toList();
-    }
-
-    public List<UUID> getOneBusShortCircuitAnalysisResultUuids() {
-        return oneBusShortCircuitAnalysisResultUuids.stream().toList();
-    }
-
-    public List<UUID> getVoltageInitResultUuids() {
-        return voltageInitResultUuids.stream().toList();
-    }
-
-    public List<UUID> getStateEstimationResultUuids() {
-        return stateEstimationResultUuids.stream().toList();
-    }
-
-    public List<UUID> getPccMinResultUuids() {
-        return pccMinResultUuids.stream().toList();
-    }
-
-    public List<UUID> getDynamicSimulationResultUuids() {
-        return dynamicSimulationResultUuids.stream().toList();
-    }
-
-    public List<UUID> getDynamicSecurityAnalysisResultUuids() {
-        return dynamicSecurityAnalysisResultUuids.stream().toList();
-    }
-
-    public List<UUID> getDynamicMarginCalculationResultUuids() {
-        return dynamicMarginCalculationResultUuids.stream().toList();
-    }
 
     public void addRemovedNodeUuid(UUID removedNodeUuid) {
         removedNodeUuids.add(removedNodeUuid);
@@ -95,56 +34,7 @@ public class DeleteNodeInfos {
         modificationGroupUuids.add(modificationGroupUuid);
     }
 
-    public void addReportUuid(UUID reportUuid) {
-        reportUuids.add(reportUuid);
-    }
-
     public void addVariantId(UUID networkUuid, String variantId) {
         variantIds.getOrDefault(networkUuid, new ArrayList<>()).add(variantId);
     }
-
-    public void addLoadFlowResultUuid(UUID loadFlowResultUuid) {
-        loadFlowResultUuids.add(loadFlowResultUuid);
-    }
-
-    public void addSecurityAnalysisResultUuid(UUID securityAnalysisResultUuid) {
-        securityAnalysisResultUuids.add(securityAnalysisResultUuid);
-    }
-
-    public void addDynamicSimulationResultUuid(UUID dynamicSimulationResultUuid) {
-        dynamicSimulationResultUuids.add(dynamicSimulationResultUuid);
-    }
-
-    public void addDynamicSecurityAnalysisResultUuid(UUID dynamicSecurityAnalysisResultUuid) {
-        dynamicSecurityAnalysisResultUuids.add(dynamicSecurityAnalysisResultUuid);
-    }
-
-    public void addDynamicMarginCalculationResultUuid(UUID dynamicMarginCalculationResultUuid) {
-        dynamicMarginCalculationResultUuids.add(dynamicMarginCalculationResultUuid);
-    }
-
-    public void addSensitivityAnalysisResultUuid(UUID sensitivityAnalysisResultUuid) {
-        sensitivityAnalysisResultUuids.add(sensitivityAnalysisResultUuid);
-    }
-
-    public void addShortCircuitAnalysisResultUuid(UUID shortCircuitAnalysisResultUuid) {
-        shortCircuitAnalysisResultUuids.add(shortCircuitAnalysisResultUuid);
-    }
-
-    public void addOneBusShortCircuitAnalysisResultUuid(UUID oneBusShortCircuitAnalysisResultUuid) {
-        oneBusShortCircuitAnalysisResultUuids.add(oneBusShortCircuitAnalysisResultUuid);
-    }
-
-    public void addVoltageInitResultUuid(UUID voltageInitResultUuid) {
-        voltageInitResultUuids.add(voltageInitResultUuid);
-    }
-
-    public void addStateEstimationResultUuid(UUID stateEstimationResultUuid) {
-        stateEstimationResultUuids.add(stateEstimationResultUuid);
-    }
-
-    public void addPccMinResultUuid(UUID pccMinResultUuid) {
-        pccMinResultUuids.add(pccMinResultUuid);
-    }
-
 }
