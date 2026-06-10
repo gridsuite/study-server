1cb684642530988153ef90a0665429f18ff3756a
Hugo Marcellin <marcellinhugo@orange.fr>
2026-06-10 15:18:51 +0200

    Fix invalidated multiroot studies blocked nodes (#1007)
    Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/SupervisionService.java
+++ src/main/java/org/gridsuite/study/server/service/SupervisionService.java
@@ -373,9 +373,14 @@ public class SupervisionService {
     public void invalidateStudy(UUID studyUuid) {
         AtomicReference<Long> startTime = new AtomicReference<>();
         startTime.set(System.nanoTime());
-        rootNetworkService.getStudyRootNetworkIds(studyUuid).forEach(rnId ->
-                studyService.invalidateStudyRootNetwork(studyUuid, rnId, SUPERVISION_USER)
-        );
+        try {
+            rootNetworkService.getStudyRootNetworkIds(studyUuid).forEach(rnId ->
+                    studyService.invalidateStudyRootNetwork(studyUuid, rnId, SUPERVISION_USER)
+            );
+        } finally {
+            var rootNodeUuid = networkModificationTreeService.getStudyRootNodeUuid(studyUuid);
+            studyService.unblockNodeTree(studyUuid, rootNodeUuid);
+        }
         notificationService.emitElementUpdated(studyUuid, SUPERVISION_USER);
         LOGGER.trace("Study {} nodes builds deleted and root node invalidated in : {} milliseconds", studyUuid, TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime.get()));
     }
