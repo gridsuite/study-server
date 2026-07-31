aa34a7b03218088ac2a1ce4ccdb929a6c13272cf
basseche <bassel.el-cheikh_externe@rte-france.com>
2026-07-31 12:19:19 +0200

    review
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/stateestimation/StateEstimationService.java
+++ src/main/java/org/gridsuite/study/server/service/stateestimation/StateEstimationService.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.study.server.service.stateestimation;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,6 +25,10 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.gridsuite.study.server.dto.ComputationType.STATE_ESTIMATION;
+
+/**
+ * @author Bassel El Cheikh <bassel.el-cheikh_externe at rte-france.com>
+ */
 
 @Service
 public class StateEstimationService extends AbstractComputationService {
