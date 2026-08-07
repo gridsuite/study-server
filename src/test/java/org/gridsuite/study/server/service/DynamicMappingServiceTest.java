f6c2a00bf42f0799faa8ff28254df624edf4c4bf
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-07 16:17:08 +0200

    fix checkstyle
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/test/java/org/gridsuite/study/server/service/DynamicMappingServiceTest.java
+++ src/test/java/org/gridsuite/study/server/service/DynamicMappingServiceTest.java
@@ -14,8 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
