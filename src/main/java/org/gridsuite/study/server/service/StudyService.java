085158aeeb37b397e8652e7018d76ba7e7ca04de
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-09-15 12:01:20 +0200

    unit tests + small refacto of the imports
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/main/java/org/gridsuite/study/server/service/StudyService.java
+++ src/main/java/org/gridsuite/study/server/service/StudyService.java
@@ -1997,6 +1997,8 @@ public class StudyService {
         List<UUID> childrenUuids = networkModificationTreeService.getChildrenUuids(targetNodeUuid);
         try {
             checkStudyContainsNode(targetStudyUuid, targetNodeUuid);
+            // TODO : to avoid those references requests this code will be moved to netmod-server once it will be able to call directory-server directly
+            // (should be added in a following ticket)
             List<ModificationReference> referenceMods = networkModificationService.getModificationReferences(modificationsUuids);
             newCompositeUuid = networkModificationService.assembleModificationsIntoComposite(modificationsUuids);
             // if some of the assembled modifications are shared, their container is now the newly created composite
