16f1a969f5c415d20d871c35c3f2c3ce6377cecf
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2026-09-23 17:04:24 +0200

    reduces backtracking
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/test/java/org/gridsuite/study/server/NetworkModificationTreeTest.java
+++ src/test/java/org/gridsuite/study/server/NetworkModificationTreeTest.java
@@ -314,7 +314,7 @@ class NetworkModificationTreeTest {
                     return new MockResponse(HttpStatus.OK.value(), Headers.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE), objectMapper.writeValueAsString(2));
                 } else if (path.matches("/v1/groups/" + MODIFICATION_GROUP_UUID_3 + "/network-modifications-count.*") && request.getMethod().equals("GET")) {
                     return new MockResponse(HttpStatus.OK.value(), Headers.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE), objectMapper.writeValueAsString(0));
-                } else if (path.matches("/v1/groups/.*/network-modifications-count.*") && request.getMethod().equals("GET")) {
+                } else if (path.startsWith("/v1/groups/") && path.contains("/network-modifications-count") && Objects.equals(request.getMethod(), "GET")) {
                     // by default nodes contain no modifications
                     return new MockResponse(HttpStatus.OK.value(), Headers.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE), objectMapper.writeValueAsString(0));
                 } else if (path.matches("/v1/groups/" + MODIFICATION_GROUP_UUID + "/.*") && request.getMethod().equals("GET")) {
